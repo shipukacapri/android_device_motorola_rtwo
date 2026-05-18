5cd751ec9bff772f3c56bfd3aeb6f2587af63d38
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-18 08:33:28 +0530

    rtwo: PixelOS BringUp (makefile)

--- custom_rtwo.mk
+++ custom_rtwo.mk
@@ -10,10 +10,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from rtwo device
 $(call inherit-product, device/motorola/rtwo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_rtwo
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_NAME := custom_rtwo
 PRODUCT_DEVICE := rtwo
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola

217ba224f1ad9ebdd32f75d690e6d51188523323
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-30 10:58:30 +0530

    rtwo: voltage bringup makefile

--- voltage_rtwo.mk
+++ voltage_rtwo.mk
@@ -10,10 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from rtwo device
 $(call inherit-product, device/motorola/rtwo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_rtwo
+# Bootanimation Resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := voltage_rtwo
 PRODUCT_DEVICE := rtwo
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
